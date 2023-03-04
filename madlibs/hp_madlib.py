453714e425f75cf812ca1e3d65f8dffc71c37359
Dylan Prole <dylan.prole@xero.com>
2023-03-04 15:02:54 +1300

    madlibs_initial_commit

---
+++ madlibs/hp_madlib.py
@@ -0,0 +1,15 @@
+# Harry Potter Madlib
+# Created by: Dylan Prole 
+# Time taken: 5 minutes
+
+number1 = input("Enter a number: ")
+adj1 = input("Enter an adjective: ")
+adj2 = input("Enter an adjective: ")
+animal_plural = input("Please enter the name of a group of animals (plural): ")
+adj3 = input("Enter an adjective: ")
+adj4 = input("Enter an adjective: ")
+noun1 = input("Enter a noun: ")
+
+hp_book1 = f'''Mr. and Mrs. Dursley, of number {number1}, Privet Drive, were {adj1} to say that they were perfectly {adj2}, thank you very much. They were the last {animal_plural} you'd expect to be involved in anything {adj3} or {adj4}, because they just didn’t hold {noun1} with such nonsense.'''
+
+print(hp_book1)
